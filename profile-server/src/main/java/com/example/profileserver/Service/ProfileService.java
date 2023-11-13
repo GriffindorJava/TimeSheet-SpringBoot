ef52a6e96c6cca2ebc64c1ac7b57b5f7473b6f03
NakamuraSuzuki <devprodev777@gmail.com>
2023-11-12 22:40:05 -0600

    profile service methods

--- profile-server/src/main/java/com/example/profileserver/Service/ProfileService.java
+++ profile-server/src/main/java/com/example/profileserver/Service/ProfileService.java
@@ -1,10 +1,14 @@
 package com.example.profileserver.Service;
 
 import com.example.profileserver.DAO.ProfileRepository;
+import com.example.profileserver.Domain.Contact;
 import com.example.profileserver.Domain.Profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProfileService {
@@ -15,7 +19,26 @@ public class ProfileService {
         this.profileRepository = profileRepository;
     }
 
-    public void createProfile(Profile profile) {
+    public Profile createProfile(Profile profile) {
         profileRepository.save(profile);
+        return profile;
     }
+
+    public Optional<Profile> getProfileById(String id) {
+        return profileRepository.findById(id);
+    }
+
+    public Contact updateContact(String id, Contact contact) {
+        Optional<Profile> profileToBeUpdated = profileRepository.findById(id);
+        profileToBeUpdated.ifPresent(profile -> {
+            profile.setContact(contact);
+            profileRepository.save(profile);
+        });
+        return contact;
+    }
+
+    public List<Profile> getAllProfiles() {
+        return profileRepository.findAll();
+    }
+
 }
