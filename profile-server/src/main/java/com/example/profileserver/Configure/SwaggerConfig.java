a84cb8acc92bb792fa792062e24aab2d3a91196d
NakamuraSuzuki <devprodev777@gmail.com>
2023-11-13 22:19:51 -0600

    update swagger2 config for 3.0.0

--- profile-server/src/main/java/com/example/profileserver/Configure/SwaggerConfig.java
+++ profile-server/src/main/java/com/example/profileserver/Configure/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.example.profileserver.Configuration;
+package com.example.profileserver.Configure;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,18 +6,23 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
+//@EnableSwagger2 //swagger 3.0.0 does not require this anymore
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.profileserver.Controller"))
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
+
 }
+// mvn spring-boot:run
+// http://localhost:11000/swagger-ui/
+// or
+// http://localhost:11000/swagger-ui/index.html
