30bd1a8f0510a46a32db64aa9f63e2b3e9a806c1
NakamuraSuzuki <devprodev777@gmail.com>
2023-11-14 16:29:02 -0600

    DailyTimesheet used in profile weeklyTimesheetTemplate

---
+++ profile-server/src/main/java/com/example/profileserver/Domain/DailyTimesheet.java
@@ -0,0 +1,17 @@
+package com.example.profileserver.Domain;
+
+public class DailyTimesheet {
+    private String weekday;
+
+    private String date;
+
+    private String startingTime;
+
+    private String endingTime;
+
+    private boolean isFloatingDay;
+
+    private boolean isVacationDay;
+
+    private boolean isHoliday;
+}
