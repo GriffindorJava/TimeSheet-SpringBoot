1645abba7e53eb8b089b98b1fbf97717e02b1376
NakamuraSuzuki <devprodev777@gmail.com>
2023-11-13 23:34:53 -0600

    timesheet-server module initialization

---
+++ timesheet-server/src/main/java/com/example/timesheetserver/TimesheetServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.timesheetserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetServerApplication.class, args);
+	}
+
+}
---
+++ timesheet-server/src/test/java/com/example/timesheetserver/TimesheetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timesheetserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
