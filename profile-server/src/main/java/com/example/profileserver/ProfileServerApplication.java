65c01946e9073920fc3eefe3de59808aab942cf7
NakamuraSuzuki <devprodev777@gmail.com>
2023-11-13 22:38:53 -0600

    eureka client config and properties setup

--- profile-server/src/main/java/com/example/profileserver/ProfileServerApplication.java
+++ profile-server/src/main/java/com/example/profileserver/ProfileServerApplication.java
@@ -2,8 +2,10 @@ package com.example.profileserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProfileServerApplication {
 
 	public static void main(String[] args) {
