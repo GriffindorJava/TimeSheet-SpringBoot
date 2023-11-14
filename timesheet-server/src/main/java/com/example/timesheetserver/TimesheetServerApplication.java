f29d405ad7ce7271c5111e17c0e9d56f49c02f86
NakamuraSuzuki <devprodev777@gmail.com>
2023-11-14 12:03:57 -0600

    annotation to enable eureka client

--- timesheet-server/src/main/java/com/example/timesheetserver/TimesheetServerApplication.java
+++ timesheet-server/src/main/java/com/example/timesheetserver/TimesheetServerApplication.java
@@ -2,8 +2,10 @@ package com.example.timesheetserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TimesheetServerApplication {
 
 	public static void main(String[] args) {
